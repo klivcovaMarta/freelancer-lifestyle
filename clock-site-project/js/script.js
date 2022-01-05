7cd76c736cb6981e7a744272b4ae10c5310d9ab9
klivcovaMarta <klivcovamarta1987@gmail.com>
2022-01-06 01:26:32 +0300

    06012021

--- clock-site-project/js/script.js
+++ clock-site-project/js/script.js
@@ -1,9 +1,10 @@
+/*<СЛАЙДЕР>****************************************************************************/
 // Инициализация слайдера Swiper
-// new Swiper('.main-slider__body');
-
-const swiper = new Swiper('.swiper', {
+const swiperOne = new Swiper('.main-slider', {
 	// Optional parameters
 	direction: 'horizontal',
+
+	// Повторяться до бесконечности
 	loop: true,
   
 	// If we need pagination
@@ -13,25 +14,74 @@ const swiper = new Swiper('.swiper', {
   
 	// Navigation arrows
 	navigation: {
-	  nextEl: '.swiper-button-next',
-	  prevEl: '.swiper-button-prev',
+	  nextEl: '.control-main-slider__arrow_next',
+	  prevEl: '.control-main-slider__arrow_prev',
 	},
   
 	// And if we need scrollbar
 	scrollbar: {
 	  el: '.swiper-scrollbar',
 	},
-  });
+	// Set to true to enable Mutation Observer on Swiper and its elements. 
+	// In this case Swiper will be updated(reinitialized) each time if you change its style(like hide/ show) or modify its child elements (like adding / removing slides)
+	observer: true,
+	//Set to true if you also need to watch Mutations for Swiper parent elements
+	observeParents: true,
+	// Кол-во слайдов, которые нужно показывать
+	slidesPerView: 1,
+	spaceBetween: 0,
+	autoHeight: true,
+	speed: 800
 
 
+});
 
 
+const swiperTwo = new Swiper('.slider-lots', {
+// Optional parameters
+direction: 'horizontal',
+
+// Повторяться до бесконечности
+ loop: true,
+
+// If we need pagination
+pagination: {
+	el: '.swiper-pagination',
+},
+
+// Navigation arrows
+navigation: {
+	nextEl: '.control-slider-lots__arrow_next',
+	prevEl: '.control-slider-lots__arrow_prev',
+},
+
+// And if we need scrollbar
+scrollbar: {
+	el: '.swiper-scrollbar',
+},
+// Set to true to enable Mutation Observer on Swiper and its elements. 
+// In this case Swiper will be updated(reinitialized) each time if you change its style(like hide/ show) or modify its child elements (like adding / removing slides)
+observer: true,
+//Set to true if you also need to watch Mutations for Swiper parent elements
+observeParents: true,
+// Кол-во слайдов, которые нужно показывать
+slidesPerView: 3,
+// Расстояние между слайдами нужно задавать так, а не с помощью отступов. 
+// Ширину каждого слайда задаем через ширину контейнера
+spaceBetween: 100,
+autoHeight: true,
+speed: 800,
+// centerSlides: true
+// centerSlidesBounds: true
+});
 
 
+  
+/*<ВЫПАДАЮЩЕЕ МЕНЮ>****************************************************************************/
 // Показываем  меню dropdown при клике на значок user на чистом JS
 let user_icon = document.querySelector('.user-header__icon');  
-user_icon.addEventListener("click", function (e) {
-	let user_menu = document.querySelector('.user-header__menu');
+let user_menu = document.querySelector('.user-header__menu');
+user_icon.addEventListener("click", function (e) {	
 	user_menu.classList.toggle('_active');
 });
 // И закрываем меню dropdown при клике на любом месте страницы
@@ -42,7 +92,7 @@ document.documentElement.addEventListener("click", function (e) {
 	}
 });
 
-
+/*<МЕНЮ-БУРГЕР>****************************************************************************/
 // Класс _active для бургера на чистом JS
 let burger = document.querySelector('.icon-menu');
 let menuBody = document.querySelector(".menu__body");  
