15c006b392afb4efb03b834ce1037cfcd4764209
klivcovaMarta <klivcovamarta1987@gmail.com>
2022-01-18 21:08:58 +0300

    18012022-ready

--- clock-site-luxtrader-project/js/script.js
+++ clock-site-luxtrader-project/js/script.js
@@ -1,6 +1,6 @@
 /*<СЛАЙДЕР>****************************************************************************/
 // Инициализация слайдера Swiper
-const swiperOne = new Swiper('.main-slider', {
+const swiperMain = new Swiper('.main-slider', {
 	// Optional parameters
 	direction: 'horizontal',
 
@@ -36,8 +36,7 @@ const swiperOne = new Swiper('.main-slider', {
 
 });
 
-
-const swiperTwo = new Swiper('.slider-lots', {
+const swiperLots = new Swiper('.slider-lots', {
 // Optional parameters
 direction: 'horizontal',
 
@@ -71,11 +70,71 @@ slidesPerView: 3,
 spaceBetween: 100,
 autoHeight: true,
 speed: 800,
-// centerSlides: true
-// centerSlidesBounds: true
+// centerSlides: true,
+// centerSlidesBounds: true,
+
+// Responsive breakpoints
+breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1    
+    },
+    // when window width is >= 480px
+    550: {
+		slidesPerView: 2,
+		spaceBetween: 20    
+    },
+	// when window width is >= 640px
+	768: {
+		slidesPerView: 3,
+		spaceBetween: 30
+	},
+    // when window width is >= 640px
+    975: {
+      slidesPerView: 3,
+      spaceBetween: 100
+    }
+  },
 });
 
-
+const swiperQuotes = new Swiper('.slider-quotes', {
+	effect: 'fade',
+	// Optional parameters
+	// direction: 'horizontal',
+	
+	// Повторяться до бесконечности
+	 loop: true,
+	
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+	},
+	
+	// Navigation arrows
+	navigation: {
+		nextEl: '.control-slider-quotes__next',	
+	},
+	
+	// And if we need scrollbar
+	scrollbar: {
+		el: '.swiper-scrollbar',
+	},
+	// Set to true to enable Mutation Observer on Swiper and its elements. 
+	// In this case Swiper will be updated(reinitialized) each time if you change its style(like hide/ show) or modify its child elements (like adding / removing slides)
+	observer: true,
+	//Set to true if you also need to watch Mutations for Swiper parent elements
+	observeParents: true,
+	// Кол-во слайдов, которые нужно показывать
+	slidesPerView: 1,
+	// Расстояние между слайдами нужно задавать так, а не с помощью отступов. 
+	// Ширину каждого слайда задаем через ширину контейнера
+	spaceBetween: 100,
+	autoHeight: true,
+	speed: 1000,
+	// centerSlides: true,
+	// centerSlidesBounds: true,	
+	
+});
   
 /*<ВЫПАДАЮЩЕЕ МЕНЮ>****************************************************************************/
 // Показываем  меню dropdown при клике на значок user на чистом JS
